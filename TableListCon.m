c00ba172a87793c1a67f272636cba87e678a85d9
dbr <dbr.onix@gmail.com>
2009-05-05 19:03:35 +0100

    Code to properly add file, or directory to list

--- TableListCon.m
+++ TableListCon.m
@@ -27,27 +27,64 @@ writeRowsWithIndexes:(NSIndexSet *)rowIndexes
     return dragOp;
 }
 
+-(void)addFileToList:(NSString*)path
+{
+    NSLog(@"addFile %@", path);
+    NSArray *components = [path pathComponents];
+    NSString *filename = [components lastObject];
+    
+    // Generate dict of new file
+    NSMutableDictionary *cfile = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                  filename, @"filename", 
+                                  [NSNumber numberWithBool:YES], @"rename",
+                                  path, @"path",
+                                  nil];
+    
+    // Add it to the tableView's array controller
+    [ArrayCon addObject:cfile];
+}
+
+-(void)addDirectoryToList:(NSString*)path
+{
+    NSDirectoryEnumerator *direnum = [[NSFileManager defaultManager]
+                                      enumeratorAtPath:path];
+    NSString *pname;
+    while (pname = [direnum nextObject])
+    {
+        if ([[pname pathExtension] isEqualToString:@"DS_Store"])
+        {
+            /* don't enumerate this directory */
+            [direnum skipDescendents];
+        }
+        else
+        {
+            [self addFileToList:pname];
+        }
+    }    
+}
+
 - (BOOL)tableView:(NSTableView*)tv
        acceptDrop:(id<NSDraggingInfo>)info
               row:(int)row
     dropOperation:(NSTableViewDropOperation)op
 {
     NSPasteboard *pboard = [info draggingPasteboard];
-    if ([[pboard types] containsObject:NSFilenamesPboardType])
+    if([[pboard types] containsObject:NSFilenamesPboardType])
     {
         NSArray *files = [pboard propertyListForType:NSFilenamesPboardType];
         for(id file in files)
         {
-            NSMutableDictionary *cfile = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                          file, @"filename", 
-                                          [NSNumber numberWithBool:YES], @"rename",
-                                          [NSURL URLWithString:@""], @"path",
-                                          nil];
-            NSLog(@"%@", cfile);
-            [[ArrayCon mutableArrayValueForKey:@"content"] addObject:cfile];
+            if(YES)
+                [self addFileToList:file];
+            else
+                [self addDirectoryToList:file];
         }
+    
+        NSLog(@"Hail Atlanta");
+        // Done all files
+        return YES;
     }
-
-    return YES;
+    
+    return NO;
 }
 @end
